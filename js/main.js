6b2615a47e963b2f418d6667c7f78ee095d53673
zxtheo <zxtheo@live.co.uk>
2020-07-06 12:36:31 +0100

    added cohesion

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
         type: Phaser.AUTO,
         width: 400,
         height: 600,
+        backgroundColor: '#52c400',
         physics: {
             default: 'arcade',
             arcade: {
@@ -23,33 +24,19 @@
     var graphics;
     function preload ()
     {
-        this.load.audio('baa', 'assets/bllleeggggaah.mp3')
-        this.load.image('sheep', 'assets/sheep.png');
+        this.load.image('sheep', 'assets/baabaa.png');
     }
 
     function create ()
     {
-
-        // cursors = this.input.keyboard.createCursorKeys();
         graphics = this.add.graphics()
         numOfSheep = 10
         var group = this.physics.add.group();
         for (i = 0; i < numOfSheep; i++){
-            // circ.push(this.add.circle(0, 0, 5).setStrokeStyle(2, 0xffff00));
-            
-   
             sheepArray.push(new Sheep(group, sheepArray, graphics));
         }
-        // const triangle = new Phaser.GameObjects.Triangle(this.physics, 100, 100, 20)
-        
-        
-        const playBaa = ()=>{
-            var rand = Math.random();
-            if (rand < 0.1){
-                this.sound.add('baa').play();
-            }
-        }
-        this.physics.add.collider(group, group, playBaa);
+
+        this.physics.add.collider(group, group);
     }
 
     function update() {
@@ -57,31 +44,5 @@
         sheepArray.forEach(sheep => {
             sheep.update();
         });
-        
-        
-
-        // var rand = Math.random();
-        
-        // if (rand < 0.01){
-        //     sheep.setVelocityY(-sheep.body.velocity.y);
-        // }
-        
-        // if (cursors.left.isDown)
-        // {
-        //     sheep.setVelocityX(-300);
-        // }
-        // else if (cursors.right.isDown)
-        // {
-        //     sheep.setVelocityX(300);
-        // }
-    
-        // if (cursors.up.isDown)
-        // {
-        //     sheep.setVelocityY(-300);
-        // }
-        // else if (cursors.down.isDown)
-        // {
-        //     sheep.setVelocityY(300);
-        // }
     }
     
--- js/sheep.js
+++ js/sheep.js
@@ -2,83 +2,61 @@ const DISTANCE = 200
 const FOV = Math.PI/2
 
 class Sheep{
-    constructor(physics, sheepArray, graphics){
+    constructor(physics, sheepArray){
         this.sprite = physics.create(Math.random()*400, Math.random()*400, "sheep");
-        this.sprite.setScale(0.015);
+        this.sprite.setScale(0.05)
         this.sprite.setCollideWorldBounds(true);
-        this.sprite.setVelocityY(100);
-        // this.sprite.setMass(Math.random()*5);
-        this.sprite.setBounce(1);
-        this.sprite.setCircle(10/0.015);
-        this.sheepArray = sheepArray;
-        this.graphics = graphics;
-        
-        this.triangle = new Phaser.Geom.Triangle.BuildEquilateral(100, 200, 200);
-        graphics.strokeTriangleShape(this.triangle)
-        
+        this.sprite.setBounce(0);
+        var radius = this.sprite.width / 2;
+        this.sprite.body.setCircle(radius)
+        this.sheepArray = sheepArray;     
         this.direction;
+
+
     }
 
-    
-
     update(){
-        // this.triangle.x = this.sprite.x
-        // this.triangle.y = this.sprite.y
-        // debugger
-        const {x:x1,y:y1} = this.sprite
-        // const {y2,y3} = this.triangle
-        // const fov = Math.PI / 2
-        // const distance = 50;
         const {x,y} = this.sprite.body.velocity;
         this.direction = Math.atan2(y, x) + Math.PI/2;
         this.sprite.rotation = this.direction;
-        /*
-                
-            x1,y1 is the x,y of the centre of the sheep (this shall cause problems later)
-        
 
-                                              --------> x2, y2 shall be here
-                                        -----
-               OO0O               -----
-             OOOO0OOO       ----
-            OOOOO0OOOO ------------------------------ This is the direction
-             OOOO0OOO       ----
-               OO0O               -----
-                                        -----
-                                              --------> x3, y3 shall be here
-
-        */
-        const triAngle = this.direction - Math.PI/2
-        var x2 = x1 + Math.cos(triAngle + FOV/2) * DISTANCE;
-        var y2 = y1 + Math.sin(triAngle + FOV/2) * DISTANCE;
-
-        var x3 = x1 + Math.cos(triAngle - FOV/2) * DISTANCE;
-        var y3 = y1 + Math.sin(triAngle - FOV/2) * DISTANCE;
-        this.triangle.setTo(x1,y1,x2,y2,x3,y3)
-        // Phaser.Geom.Triangle.CenterOn(this.triangle, x1, y1)
-        // Phaser.Geom.Triangle.SetAngle(this.triangle, this.direction)
-        this.graphics.strokeTriangleShape(this.triangle)
-
-        
-        // this.view();
-        // var rand = Math.random();
-        
-        // if (rand < 0.01){
-        //     this.sprite.setVelocityY(-this.sprite.body.velocity.y);
-        // }
+        var cohesion = this.multiply(this.cohesion(), 5);
+        this.sprite.setVelocity(cohesion.x, cohesion.y);
     }
 
-    view(){
-        this.sheepArray.forEach(sheep =>{
-            var distance = Phaser.Math.Distance.BetweenPoints(sheep.sprite, this.sprite)
-            // if(distance == 0 || distance > this.fieldOfView){
-            //     return false
-            // }s
-            var {x,y} = sheep.sprite;
-            var relPoint = Phaser.Actions.RotateAround({x,y}, this.sprite, this.direction)
-            // this.graphics.fillCircleShape(new Phaser.Geom.Circle(relPoint.x, relPoint.y, 5))
-            var angle = Phaser.Math.Angle.BetweenPoints(sheep.sprite, this.sprite)
-            // console.log(relPoint)
+    cohesion(){
+        var averagePosition = {x:0, y:0}
+        this.sheepArray.forEach(sheep => {
+            var {x,y} = sheep.sprite
+            averagePosition.x += x;
+            averagePosition.y += y;
         });
+        
+        var sheepNo =  this.sheepArray.length;
+        averagePosition.x = averagePosition.x / sheepNo;
+        averagePosition.y = averagePosition.y / sheepNo;
+
+        var normSheep = this.subtract(averagePosition, this.sprite.body);
+        var magnitude = this.magnitude(normSheep);
+
+        var cohesionVector = this.divide(normSheep, magnitude);
+
+        return cohesionVector;
+    }
+
+    subtract(a, b){
+        return {x:a.x - b.x, y:a.y - b.y};
+    }
+
+    magnitude(a){
+        return Math.sqrt(a.x*a.x + a.y*a.y);
+    }
+
+    divide(a, b){
+        return{x:a.x/b, y:a.y/b};
+    }
+
+    multiply(a, b){
+        return{x:a.x*b, y:a.y*b};
     }
 }
